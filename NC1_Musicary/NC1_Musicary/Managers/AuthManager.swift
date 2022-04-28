288944f98f6c1f9dc9dcb8ea16a0d005a21e303e
seongmin221 <seongmin221@naver.com>
2022-04-28 16:34:46 +0900

    update gitignore

--- NC1_Musicary/NC1_Musicary/Managers/AuthManager.swift
+++ NC1_Musicary/NC1_Musicary/Managers/AuthManager.swift
@@ -13,6 +13,7 @@ final class AuthManager {
     struct Constants {
         static let clientID = "a35f198bf41e4bd1977026c8eafe34a1"
         static let clientSecret = "b5c83eae85c84a7c977dcb481e33cc11"
+        
     }
     
     private init() {}
